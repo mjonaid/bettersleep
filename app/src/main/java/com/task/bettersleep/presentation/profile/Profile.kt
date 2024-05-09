9c43f4ff94adde4451c6efbd18a104a4892e0398
mjonaid <mj4718927@gmail.com>
2024-05-09 14:49:42 +0500

    profilektcan be added

--- app/src/main/java/com/task/bettersleep/presentation/profile/Profile.kt
+++ app/src/main/java/com/task/bettersleep/presentation/profile/Profile.kt
@@ -2,22 +2,23 @@ package com.task.bettersleep.presentation.profile
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.task.bettersleep.R
+import com.task.bettersleep.databinding.FragmentProfileBinding
 
-class Profile : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+class Profile : Fragment(R.layout.fragment_profile) {
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentProfileBinding.bind(view)
+         binding.profiletv.text = "Hello World"
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
+
+
